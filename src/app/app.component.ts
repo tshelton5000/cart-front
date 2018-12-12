ef6e0f3428df3c83b6916a744ac35f7b3a591cea
admin <ts70@evansville.edu>
2018-12-12 16:16:41 -0500

    first frontend commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ProductsService} from './products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+  productData: any;
+  cart = []
+
+  constructor(private products: ProductsService){}
+
+  //
+  toggleProductInCart(product){
+    this.cart.includes(product) ? this.cart = this.cart.filter(el => el != product) : this.cart.push(product);
+    console.log(this.cart);
+  }
+  //
+
+  ngOnInit(){
+    this.products.getProducts()
+    .subscribe((res:any) => {
+      this.productData = res.products;
+      console.log('this.productdata', this.productData);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {ProductsService} from './products.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
